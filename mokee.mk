75bb0fb6cc7aff6b9c1fe4d4f5c7377dee23805e
martincz <martincz@mokeedev.com>
2015-02-08 13:47:25 +0800

    serranoltexx: mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -1,8 +1,8 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
 
@@ -12,4 +12,4 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte
 
-PRODUCT_NAME := cm_serranoltexx
+PRODUCT_NAME := mk_serranoltexx
